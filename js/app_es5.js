c301cdcb1a41ae418a8fa6bdfed9454ac8228d2e
Vlad Choi <letskeepcoding@gmail.com>
2019-08-11 14:03:16 +0300

    JS: создал два класса Book и UI, написал функцию добавления ээкземпляра Book в список UI, написал функцию отчистки полей формы после нажатия на конпку Добавить книгув

--- js/app_es5.js
+++ js/app_es5.js
@@ -1,5 +1,5 @@
 //Book Constructor
-function Book(title, author isbn) {
+function Book(title, author, isbn) {
 	this.title = title;
 	this.author = author;
 	this.isbn = isbn;
@@ -7,8 +7,28 @@ function Book(title, author isbn) {
 
 //UI Constructor
 function UI() {}
+UI.prototype.addBookToList = function(book) {
+	const list = document.getElementById('book-list');
+	const rowElem = document.createElement('tr');
+	rowElem.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.isbn}<td><a href="#" class="delete">X</a></td>`
+	list.appendChild(rowElem);
+};
+UI.prototype.clearInputs = function() {
+	document.getElementById('title').value = '';
+	document.getElementById('author').value = '';
+	document.getElementById('isbn').value = '';
+};
 
 document.getElementById('book-form').addEventListener('submit', function(e) {
-	console.log('test');
+	const title = document.getElementById('title').value,
+				author = document.getElementById('author').value,
+				isbn = document.getElementById('isbn').value;
+
+	const book = new Book(title, author, isbn);
+
+	const ui = new UI();
+	ui.addBookToList(book);
+	ui.clearInputs();
+
 	e.preventDefault();
 });
