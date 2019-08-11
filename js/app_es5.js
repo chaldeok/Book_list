ff44d7dbc6b83773195f99eceafe0f095c84bfd7
Vlad Choi <letskeepcoding@gmail.com>
2019-08-11 12:50:10 +0300

    HTML: сверстал интерфейс, CSS: добавил стили для интерфейса

---
+++ js/app_es5.js
@@ -0,0 +1,14 @@
+//Book Constructor
+function Book(title, author isbn) {
+	this.title = title;
+	this.author = author;
+	this.isbn = isbn;
+}
+
+//UI Constructor
+function UI() {}
+
+document.getElementById('book-form').addEventListener('submit', function(e) {
+	console.log('test');
+	e.preventDefault();
+});
