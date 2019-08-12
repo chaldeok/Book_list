f0255da2944f10536b122396d2f23754887dc411
Vlad Choi <letskeepcoding@gmail.com>
2019-08-12 07:15:55 +0300

    JS: написал функцию валидации полей формы, функцию отображения сообщения о том, что надо заполнить все поля формы и о том, что книга добавлена

--- js/app_es5.js
+++ js/app_es5.js
@@ -10,8 +10,20 @@ function UI() {}
 UI.prototype.addBookToList = function(book) {
 	const list = document.getElementById('book-list');
 	const rowElem = document.createElement('tr');
-	rowElem.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.isbn}<td><a href="#" class="delete">X</a></td>`
+	rowElem.innerHTML = `<td>${book.title}</td><td>${book.author}</td><td>${book.isbn}<td><a 
+	href="#" class="delete">X</a></td>`;
 	list.appendChild(rowElem);
+};
+UI.prototype.showAlert = function(message, className) {
+	const div = document.createElement('div');
+	div.className = `alert ${className}`;
+	div.appendChild(document.createTextNode(message));
+	const container = document.querySelector('.container');
+	const table = document.querySelector('.table');
+	container.insertBefore(div, table);
+	setTimeout(function() {
+		document.querySelector('.alert').remove();
+	}, 3000);
 };
 UI.prototype.clearInputs = function() {
 	document.getElementById('title').value = '';
@@ -27,8 +39,13 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
 	const book = new Book(title, author, isbn);
 
 	const ui = new UI();
-	ui.addBookToList(book);
-	ui.clearInputs();
+	if(title === '' || author === '' || isbn === '') {
+		ui.showAlert('Заполните, пожалуйста, все поля формы', 'error');
+	} else {
+		ui.addBookToList(book);
+		ui.showAlert('Книга добавлена', 'success');
+		ui.clearInputs();
+	}
 
 	e.preventDefault();
 });
